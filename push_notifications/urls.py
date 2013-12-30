a9c75db40211e179540c926ef2950a8d27811bec
Boris Savic <boris.savic@xlab.si>
2013-12-30 13:43:02 +0100

    Added missing base_name to Router.

--- push_notifications/urls.py
+++ push_notifications/urls.py
@@ -5,8 +5,8 @@ from rest_framework.routers import DefaultRouter
 from push_notifications.views import GCMDeviceDetail, APNSDeviceDetail
 
 router = DefaultRouter()
-router.register(r'gcm', GCMDeviceDetail)
-router.register(r'apns', APNSDeviceDetail)
+router.register(r'gcm', GCMDeviceDetail, base_name='gcm')
+router.register(r'apns', APNSDeviceDetail, base_name='apns')
 
 urlpatterns = patterns('',
                        url(r'^', include(router.urls)),
